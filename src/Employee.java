831b4585ae69820dda6a1725d4a25b3e5c7be340
ics-bs <bjorn.svensson@ics.lu.se>
2018-08-20 12:31:28 +0200

    Added fields + access methods
    Added empID and name for Employee, name and budget for department. Generated getter and setter (access) methods.

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,18 @@
 
 public class Employee {
-
+	private String empID;
+	private int salary;
+	
+	public String getEmpID() {
+		return empID;
+	}
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 }
